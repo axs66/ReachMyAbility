c531f6f8c20bcaabf47d1b06ee56563a537d055b
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-05 20:58:12 +0800

    Update WCPluginsHeader.h

--- WCPluginsHeader.h
+++ WCPluginsHeader.h
@@ -1,28 +1,6 @@
-// WCPluginsHeader.h
-
-// 已有声明
-@class WCPluginsMgr;
-
-// 添加以下声明解决编译错误
-@interface WCTableViewManager : NSObject
-- (id)getSectionAt:(NSInteger)index;
-@end
-
-@interface WCTableViewSectionManager : NSObject
-- (void)addCell:(id)cell;
-@end
-
-@interface WCTableViewCellManager : NSObject
-+ (instancetype)normalCellForSel:(SEL)sel
-                          target:(id)target
-                       leftImage:(UIImage *)leftImage
-                           title:(NSString *)title
-                           badge:(id)badge
-                      rightValue:(id)rightValue
-                      rightImage:(UIImage *)rightImage
-                withRightRedDot:(BOOL)redDot
-                        selected:(BOOL)selected;
-@end
-
-@interface MoreViewController : UIViewController
+@interface WCPluginsMgr : NSObject
+// 这里添加你遇到的 'registerControllerWithTitle:version:controller:' 方法声明
+- (void)registerControllerWithTitle:(NSString *)title
+                            version:(NSString *)version
+                         controller:(NSString *)controller;
 @end
