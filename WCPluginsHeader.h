3cf49b8a52c2ab6adf25b6ca793dee1aed483349
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-05 21:31:40 +0800

    Delete WCPluginsHeader.h

--- WCPluginsHeader.h
+++
@@ -1,6 +0,0 @@
-@interface WCPluginsMgr : NSObject
-// 这里添加你遇到的 'registerControllerWithTitle:version:controller:' 方法声明
-- (void)registerControllerWithTitle:(NSString *)title
-                            version:(NSString *)version
-                         controller:(NSString *)controller;
-@end
